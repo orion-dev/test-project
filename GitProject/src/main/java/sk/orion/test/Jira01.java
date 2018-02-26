755819c23a65aedd9c772ac5aee808438fa086bb
Milos <Milos@Milos-PC>
2018-02-26 12:30:52 +0100

    adding funtionality after jira-02

--- GitProject/src/main/java/sk/orion/test/Jira01.java
+++ GitProject/src/main/java/sk/orion/test/Jira01.java
@@ -5,6 +5,7 @@ public class Jira01 {
 	@Override
 	public String toString() {
 		System.out.println("this is modified funcionality");
+		System.out.println("modified funcionality after jira02");
 		return "jira-01";
 	}
 }
