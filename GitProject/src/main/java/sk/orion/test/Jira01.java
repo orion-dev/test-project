506abbd5155418cafc78afe979e14886cb0060e3
Milos <Milos@Milos-PC>
2018-02-26 12:44:05 +0100

    jira03 - modified jira01

--- GitProject/src/main/java/sk/orion/test/Jira01.java
+++ GitProject/src/main/java/sk/orion/test/Jira01.java
@@ -4,7 +4,7 @@ public class Jira01 {
 
 	@Override
 	public String toString() {
-		System.out.println("this is modified funcionality");
+		System.out.println("this is modified funcionality from jira03");
 		System.out.println("modified funcionality after jira02");
 		return "jira-01";
 	}
