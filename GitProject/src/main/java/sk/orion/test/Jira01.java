1557b6262548eb4d2066bc09302725335c9852e6
Milos <Milos@Milos-PC>
2018-02-26 15:41:50 +0100

    github branch changes

--- GitProject/src/main/java/sk/orion/test/Jira01.java
+++ GitProject/src/main/java/sk/orion/test/Jira01.java
@@ -7,6 +7,7 @@ public class Jira01 {
 		System.out.println("this is modified funcionality from jira03");
 		System.out.println("funcionality from master and jira03");
 		System.out.println("modified funcionality after jira02");
+		System.out.println("modified funcionality to github branch");
 		return "jira-01";
 	}
 }
