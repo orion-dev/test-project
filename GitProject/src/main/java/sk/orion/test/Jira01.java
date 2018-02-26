107b6d8f2decf5c9cdad52b96f307ad61525529e
Milos <Milos@Milos-PC>
2018-02-26 11:07:26 +0100

    jira-01

---
+++ GitProject/src/main/java/sk/orion/test/Jira01.java
@@ -0,0 +1,9 @@
+package sk.orion.test;
+
+public class Jira01 {
+
+	@Override
+	public String toString() {
+		return "jira-01";
+	}
+}
--- GitProject/src/main/java/sk/orion/test/MainClass.java
+++ GitProject/src/main/java/sk/orion/test/MainClass.java
@@ -4,5 +4,6 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println("hello git");
+		System.out.println("this is task from " + new Jira01());
 	}
 }
