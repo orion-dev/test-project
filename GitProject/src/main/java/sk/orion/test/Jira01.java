69471febe16d087847f63f74c6e011a2a74ea7e5
Milos <Milos@Milos-PC>
2018-02-26 12:48:00 +0100

    jira03 - added func

--- GitProject/src/main/java/sk/orion/test/Jira01.java
+++ GitProject/src/main/java/sk/orion/test/Jira01.java
@@ -5,6 +5,7 @@ public class Jira01 {
 	@Override
 	public String toString() {
 		System.out.println("this is modified funcionality from jira03");
+		System.out.println("added funcionality from jira03");
 		System.out.println("modified funcionality after jira02");
 		return "jira-01";
 	}
